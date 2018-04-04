2e6ed89f58b9b4e52cc2aa90e003020bcd17b5ce
Joe <josephdean@kpmg.com>
2018-04-04 17:24:00 -0400

    new data

--- code/data_upload.R
+++ code/data_upload.R
@@ -10,6 +10,8 @@ week5<- read_csv("./data/ShowdownDataByMatch-detailed (4).csv")
 week6<- read_csv("./data/ShowdownDataByMatch-detailed (5).csv")
 week7<- read_csv("./data/ShowdownDataByMatch-detailed (6).csv")
 
+complete_matchdata <- read_csv("C:/Users/josephdean/Desktop/mygit/billsPC/data/complete_matchdata.csv")
+
 week1$week <- 1
 week2$week <- 2
 week3$week <- 3
@@ -21,12 +23,16 @@ week7$week <- 7
 all_weeks<- bind_rows(week1,week2,week3,week4,week5,week6,week7)
 all_weeks<- all_weeks[,-c(3:8)] #Dropping team that's not brought.  Need to think about how to model this
 
+complete_matchdata<- complete_matchdata[,-c(4:9)]
+
 require(reshape2)
 
 data_long<- melt(all_weeks, id.vars=c("matchID", "Win", "week"))
+matchdata_long <- melt(complete_matchdata, id.vars=c("matchID", "Player", "playerWin"))
 
 colnames(data_long)[5]<- "mon"
+colnames(matchdata_long)[5] <- "mon"
 
-write_csv(data_long, "./data/modeldata.csv")
+write_csv(matchdata_long, "./data/modeldata.csv")
 
 
