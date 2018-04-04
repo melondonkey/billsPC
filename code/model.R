d48f0e9f58d386490f7cf7a5f9d898fdbdf67b11
Joe <josephdean@kpmg.com>
2018-04-04 17:30:57 -0400

    changed back
    why is everything now flipped?

--- code/model.R
+++ code/model.R
@@ -4,7 +4,7 @@ require(ggplot2)
 df <- read_csv("./data/modeldata.csv")
 
 #Simple model with random intercecpts for each mon
-mod1<- glmer(playerWin ~ (1|mon) + (1|Player) ,
+mod1<- glmer(playerWin ~ (1|mon)  ,
             data=df,
             family="binomial"
             )
@@ -13,22 +13,22 @@ mod1<- glmer(playerWin ~ (1|mon) + (1|Player) ,
 summary(mod1)
 
 ##Process model data, extracting standard errors as well
-effects <- data.frame(ranef(mod1)$Player)
-std_errors <- data.frame(se.ranef(mod1)$Player)
-effects$Player <- rownames(effects)
+effects <- data.frame(ranef(mod1)$mon)
+std_errors <- data.frame(se.ranef(mod1)$mon)
+effects$mon <- rownames(effects)
 colnames(effects)[1] <- "estimate"
-std_errors$Player <- rownames(std_errors)
+std_errors$mon <- rownames(std_errors)
 colnames(std_errors)[1] <- "se"
 
-parameters <- effects %>% left_join(std_errors, by="Player") %>% 
+parameters <- effects %>% left_join(std_errors, by="mon") %>% 
   mutate(lower_1se = estimate - se,
          upper_1se = estimate + se
          )
 
 #Very rough effect plot
-ggplot(parameters, aes(x=Player, y=estimate)) + geom_text(aes(label=Player), check_overlap = TRUE) +
+ggplot(parameters, aes(x=mon, y=estimate)) + geom_text(aes(label=mon), check_overlap = TRUE) +
   geom_hline(yintercept = 0, color="red") + ylab("Effect Estimate- Above Red Line is Good") +
-  geom_linerange(aes(x=Player, ymin=lower_1se, ymax=upper_1se), alpha=.2, color="blue") +
+  geom_linerange(aes(x=mon, ymin=lower_1se, ymax=upper_1se), alpha=.2, color="blue") +
   theme(axis.title.x=element_blank(),
         axis.text.x=element_blank(),
         axis.ticks.x=element_blank())
